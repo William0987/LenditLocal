89002fa02f3b90b409818c6c427fa0bad075a9d0
William0987 <williamwsianto@gmail.com>
2024-09-18 20:32:46 -0400

    listings model and update listings model data

--- Backend/src/controllers/listings.js
+++ Backend/src/controllers/listings.js
@@ -1,24 +1,36 @@
 const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 
-const ListingsSchema = require("../models/listings");
+const ListingModel = require("../models/listings");
 
 const seedListings = async (req, res) => {
   try {
-    await ListingsSchema.deleteMany();
+    await ListingModel.deleteMany();
 
-    await ListingsSchema.create([
+    await ListingModel.create([
       {
         _id: "64d0f3f75676c304033d8c89",
         listing_id: uuidv4(),
+        title: "My beloved bike",
+        description: `I’m too busy with my coding bootcamp to ride it. Feel free to borrow it on weekends`,
+        type: "loan",
+        date_available_from: `${new Date()}`,
+        date_available_to: "2022-09-30",
+        image_url:
+          "https://images.immediate.co.uk/production/volatile/sites/21/2021/03/20210317_SB_5DSR_MG_4042-4cbecec.jpg?quality=90&resize=768%2C574",
       },
       {
-        _id: "64d0f3f75676c304033d8c8a",
+        _id: "64d0f3f75676c304033d8c90",
         listing_id: uuidv4(),
+        title: "Onions",
+        description: `Onions are a rich source of fiber and prebiotics, which are necessary for optimal gut health. I bought way too many onions. Giving away for free`,
+        type: "free",
+        date_available_from: `${new Date()}`,
+        date_available_to: "2022-08-30",
+        image_url:
+          "https://www.almanac.com/sites/default/files/styles/or/public/image_nodes/onions.jpg?itok=NqLGNDHS",
       },
     ]);
-    console.log("seeded");
-
     res.json({ status: "ok", msg: "seeding successful" });
   } catch (error) {
     console.log(error.message);
--- Backend/src/models/listings.js
+++ Backend/src/models/listings.js
@@ -1,11 +1,19 @@
 
 const mongoose = require("mongoose");
-const ListingsSchema = new mongoose.Schema(
+const ListingSchema = new mongoose.Schema(
   {
     listing_id: { type: String, required: true, minLength: 1, maxLength: 50 },
     created_at: { type: Date, default: Date.now },
+    title: { type: String, required: true, minLength: 1, maxLength: 100 },
+    description: { type: String, required: true },
+    type: { type: String, required: true, enums: ["free", "loan"] },
+    date_available_from: { type: Date, default: Date.now },
+    date_available_to: { type: Date },
+    image_url: { type: String },
   },
-  { collection: "test" }
+  { collection: "listings" }
 );
 
-module.exports = mongoose.model("Listings", ListingsSchema);
+const Listing = mongoose.model("Listing", ListingSchema);
+
+module.exports = Listing;
