5e825ace5d52d0395ecea30d8d387e04adcdb078
William0987 <williamwsianto@gmail.com>
2024-09-19 20:36:23 -0400

    registration and sign-in page

--- Frontend/src/pages/Registration.jsx
+++ Frontend/src/pages/Registration.jsx
@@ -1,18 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import useFetch from "../hooks/useFetch";
 import TopBar from "../components/TopBar";
 import Grid from "@mui/material/Unstable_Grid2";
-import { Container, Typography, Box } from "@mui/material";
+import { Container, Typography, Box, TextField } from "@mui/material";
+import Btn from "../components/Btn";
 
 const Registration = () => {
+  const fetchData = useFetch();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerUser = async () => {
+    const res = await fetchData("/auth/register", "PUT", {
+      email,
+      password,
+    });
+
+    if (res.ok) {
+      setEmail("");
+      setPassword("");
+      props.setShowLogin(true);
+    } else {
+      console.log(res.data);
+    }
+  };
+
   return (
     <>
       <TopBar></TopBar>
 
       <Container maxWidth="lg">
-        <Box>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 3, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
           <Grid container>
             <Grid xs={12} style={{ borderStyle: "solid" }}>
-              <Typography textAlign="center">Registration</Typography>
+              <Typography textAlign="center">
+                Register For an Account
+              </Typography>
+              <div>
+                <TextField
+                  label="Required"
+                  variant="outlined"
+                  defaultValue="Email"
+                />
+              </div>
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  label="Required"
+                  variant="outlined"
+                  defaultValue="Password"
+                  multiline
+                />
+              </div>
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  label="Required"
+                  variant="outlined"
+                  defaultValue="Confirm Password"
+                />
+              </div>
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  label="Zip Code"
+                  variant="outlined"
+                  defaultValue="Enter Your Location"
+                />
+              </div>
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  label="Required"
+                  variant="outlined"
+                  defaultValue="District"
+                />
+              </div>
+              <Btn onClick={registerUser}>Register</Btn>
             </Grid>
           </Grid>
         </Box>
--- Frontend/src/pages/SignIn.jsx
+++ Frontend/src/pages/SignIn.jsx
@@ -1,23 +1,61 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import useFetch from "../hooks/useFetch";
 import TopBar from "../components/TopBar";
 import Grid from "@mui/material/Unstable_Grid2";
-import { Container, Typography, Box } from "@mui/material";
+import { Container, Typography, Box, TextField } from "@mui/material";
 import Btn from "../components/Btn";
+import UserContext from "../context/user";
 
-const SignIn = () => {
+const SignIn = (props) => {
+  const userCtx = useContext(UserContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const fetchData = useFetch();
+  const handleLogin = async () => {
+    const res = await fetchData("/auth/login", "POST", { email, password });
+    if (res.ok) {
+      userCtx.setAccessToken(res.data.access);
+      const decoded = jwtDecode(res.data.access);
+      userCtx.setRole(decoded.role);
+    } else {
+      alert(JSON.stringify(res.data));
+    }
+  };
+
   return (
     <>
       <TopBar></TopBar>
 
       <Container maxWidth="lg">
-        <Box>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
           <Grid container>
             <Grid xs={7} style={{ borderStyle: "solid" }}>
               <Typography textAlign="center">Sign-in</Typography>
             </Grid>
             <Grid xs={5} style={{ borderStyle: "solid" }}>
               <Typography textAlign="center">Sign-in</Typography>
-              <Btn>Sign</Btn>
+              <TextField
+                id="outlined-basic"
+                label="Required"
+                variant="outlined"
+                defaultValue="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                id="outlined-basic"
+                label="Required"
+                variant="outlined"
+                defaultValue="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <Btn onClick={handleLogin}>Login</Btn>
               <Btn isBrown={true} width={15}>
                 Cancel
               </Btn>
